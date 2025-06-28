36f655c388adc9c67f63950010536d555ed91ad5
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-06-28 01:11:19 -0600

    Removed 3-chef-claude project from commit.

--- react/3-chef-claude/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-    return (
-        <div className="header">
-            <img src="./chef-claude-icon.png" alt="Chef Claude Icon" />
-            <span>Chef Claude</span>
-        </div>
-    );
-}
